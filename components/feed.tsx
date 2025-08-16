5c34af9463cb03c8da1017d6d2cf5637ef822752
Lucas Galdino Belfort Matos <lgmatos@w3haus.local>
2025-08-16 15:46:24 -0300

    remoção '

--- components/feed.tsx
+++ components/feed.tsx
@@ -30,7 +30,7 @@ export default function Feed(){
                 priority
             />
             <p className="text-sm mt-4">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
             <div className="w-full flex flex-row gap-4 items-center mt-4">
                 <div className="flex flex-row gap-1 items-center">
