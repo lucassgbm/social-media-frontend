80dc4d3a62edbf047cecc60dfa16d77b02e550b5
Lucas Galdino Belfort Matos <lgmatos@w3haus.local>
2025-08-19 19:07:52 -0300

    correção na tipação dos ícones

--- components/icons/airplane.tsx
+++ components/icons/airplane.tsx
@@ -1,4 +1,4 @@
-export default function AirPlaneIcon(props: { className: string | undefined; }){
+export default function AirPlaneIcon(props: { className?: string; }){
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={ props.className ? props.className : "" }>
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 12 3.269 3.125A59.769 59.769 0 0 1 21.485 12 59.768 59.768 0 0 1 3.27 20.875L5.999 12Zm0 0h7.5" />
--- components/icons/close.tsx
+++ components/icons/close.tsx
@@ -1,4 +1,4 @@
-export default function CloseIcon(props: { className: string | undefined; }) {
+export default function CloseIcon(props: { className?: string; }) {
     return (
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`size-6 ${props.className || ""}`} >
             <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
