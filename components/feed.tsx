7dde052323dd5ccc5b8acce8e7c5e2dadc36e308
Lucas Galdino Belfort Matos <lgmatos@w3haus.local>
2025-09-15 17:35:00 -0300

    adição da key no feed

--- components/feed.tsx
+++ components/feed.tsx
@@ -47,48 +47,50 @@ export default function Feed({ feed }: FeedProps) {
           : null;
 
         return (
-          <Container key={post.id} className="mb-4">
-            <div className="flex flex-row gap-4 items-center mb-4">
-              {imageUser && (
+          <div key={post.id}>
+            <Container key={post.id} className="mb-4">
+              <div className="flex flex-row gap-4 items-center mb-4">
+                {imageUser && (
+                  <Image
+                    src={imageUser}
+                    alt="Foto de perfil"
+                    className="rounded-full"
+                    width={50}
+                    height={50}
+                    unoptimized
+                  />
+                )}
+                <div className="flex flex-col">
+                  <span className="text-sm font-semibold">{post.user.name}</span>
+                  <span className="text-sm font-normal text-gray-400">{post.created_at}</span>
+                </div>
+              </div>
+
+              {imagePost && (
                 <Image
-                  src={imageUser}
-                  alt="Foto de perfil"
-                  className="rounded-full"
-                  width={50}
-                  height={50}
+                  src={imagePost}
+                  alt="Imagem do Post"
+                  className="w-full rounded"
+                  width={500}
+                  height={500}
                   unoptimized
                 />
               )}
-              <div className="flex flex-col">
-                <span className="text-sm font-semibold">{post.user.name}</span>
-                <span className="text-sm font-normal text-gray-400">{post.created_at}</span>
-              </div>
-            </div>
 
-            {imagePost && (
-              <Image
-                src={imagePost}
-                alt="Imagem do Post"
-                className="w-full rounded"
-                width={500}
-                height={500}
-                unoptimized
-              />
-            )}
+              <p className="text-sm mt-4">{post.description}</p>
 
-            <p className="text-sm mt-4">{post.description}</p>
-
-            <div className="w-full flex flex-row gap-4 items-center mt-4">
-              <div className="flex flex-row gap-1 items-center">
-                <HeartIcon />
-                <span className="text-sm font-semibold">{post.likes.count}</span>
+              <div className="w-full flex flex-row gap-4 items-center mt-4">
+                <div className="flex flex-row gap-1 items-center">
+                  <HeartIcon />
+                  <span className="text-sm font-semibold">{post.likes.count}</span>
+                </div>
+                <div className="flex flex-row gap-1 items-center">
+                  <MessageIcon />
+                  <span className="text-sm font-semibold">{post.comments.count}</span>
+                </div>
               </div>
-              <div className="flex flex-row gap-1 items-center">
-                <MessageIcon />
-                <span className="text-sm font-semibold">{post.comments.count}</span>
-              </div>
-            </div>
-          </Container>
+            </Container>
+          </div>
         );
       })}
     </>
