f41e377a0db0fc936b53c35cbfade2b0661e7b2f
Gilvan <ti.gilvan@gmail.com>
2025-09-29 19:34:55 -0300

    Validando url

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 const url = process.env.NEXT_PUBLIC_STORAGE_API || "CHANGEURL/public/storage/";
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL(process.env.NEXT_PUBLIC_STORAGE_API + '**')],
+    remotePatterns: [new URL(url + '**')],
   },
   eslint: {
     ignoreDuringBuilds: true,
