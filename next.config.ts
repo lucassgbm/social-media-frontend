180bcb765a212daa6a398d45b8439bfa096f6e8c
Gilvan <ti.gilvan@gmail.com>
2025-09-29 19:46:46 -0300

    AJuste no config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const url = process.env.NEXT_PUBLIC_STORAGE_API || "CHANGEURL/public/storage/";
+const url = process.env.NEXT_PUBLIC_STORAGE_API ?? "CHANGEURL/public/storage/";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL(url + '**')],
