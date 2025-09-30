018afc7f7205e8d2572442cb27c372cb599360c8
Gilvan <ti.gilvan@gmail.com>
2025-09-30 15:15:48 -0300

    Ajuste no login

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -3,70 +3,71 @@
 import Image from "next/image";
 import HeaderLogin from "../../../../components/login/header-login";
 import { useState } from "react";
-import { useRouter } from "next/navigation";
+//import { useRouter } from "next/navigation";
 import { auth } from "../../../api/services/auth";
 import Toaster from "../../../../components/toaster";
 import FormButtom from "../../../../components/form-buttom";
 import LoadingSpinner from "../../../../components/loading-spinner";
 
-export default function Home(){
-const [login, setLogin] = useState<{email: string, password: string}>({
-    email: '',
-    password: ''
-});
+export default function Home() {
+    const [login, setLogin] = useState<{ email: string, password: string }>({
+        email: '',
+        password: ''
+    });
 
-const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
-const [toaster, setToaster] = useState({
-  show: false,
-  message: "",
-});
+    const [toaster, setToaster] = useState({
+        show: false,
+        message: "",
+    });
 
-const router = useRouter();
+    //const router = useRouter();
 
-async function handleSubmit(e: React.FormEvent)  {
-    e.preventDefault();
+    async function handleSubmit(e: React.FormEvent) {
+        e.preventDefault();
 
-    setLoading(true);
-    if(login.email === '' || login.password === '') {
-        setLoading(false);
-        setToaster({...toaster, show: true, message: 'Preencha todos os campos'});
-        return;
+        setLoading(true);
+        if (login.email === '' || login.password === '') {
+            setLoading(false);
+            setToaster({ ...toaster, show: true, message: 'Preencha todos os campos' });
+            return;
+        }
+        try {
+            const response = await auth(login.email, login.password);
+
+            setLoading(false);
+            setToaster({ ...toaster, show: true, message: ' Login realizado com sucesso! Redirecionando...' });
+
+            window.location.href = '/social-media/';
+            // router.push('/social-media/');
+        } catch (err: any) {
+
+            console.log(err);
+            setLoading(false);
+            setToaster({ ...toaster, show: true, message: err.response.data.message });
+        }
     }
-    try {
-        const response = await auth(login.email, login.password);
-      
-        setLoading(false);
-        setToaster({...toaster, show: true, message:' Login realizado com sucesso! Redirecionando...'});
-
-        router.push('/social-media/');
-    } catch (err: any) {
-
-        console.log(err);
-        setLoading(false);
-        setToaster({...toaster, show: true, message: err.response.data.message});
-    }
-}
-    return(
+    return (
         <div className="flex sm:flex-row flex-col">
 
             <div className="sm:w-1/3 text-white flex flex-col w-full bg-neutral-950 h-screen items-center justify-center p-4">
-                
+
                 <HeaderLogin />
 
                 <h1 className="flex flex-end text-lg font-semibold mb-4">Seja bem vindo</h1>
 
                 <div className="w-[80%] flex flex-col">
-                    <form onSubmit={handleSubmit}> 
+                    <form onSubmit={handleSubmit}>
                         <div className="flex flex-col mb-2">
                             <label className="font-semibold text-xs mb-2">Email</label>
 
-                            <input className="w-full text-sm text-gray-700 p-3 bg-white focus:outline-blue-400 rounded-sm" 
-                                type="text" 
+                            <input className="w-full text-sm text-gray-700 p-3 bg-white focus:outline-blue-400 rounded-sm"
+                                type="text"
                                 placeholder="Digite o seu e-mail"
                                 onChange={(e) => {
-                                    setLogin({...login, email: e.target.value})
-                                    setToaster({...toaster, show: false})
+                                    setLogin({ ...login, email: e.target.value })
+                                    setToaster({ ...toaster, show: false })
                                 }}
                             ></input>
                         </div>
@@ -75,18 +76,18 @@ async function handleSubmit(e: React.FormEvent)  {
 
                             <label className="font-semibold text-xs mt-4 mb-2">Password</label>
 
-                            <input className="w-full text-sm text-gray-700 p-3 bg-white focus:outline-blue-400 rounded-sm" 
-                                type="password" 
+                            <input className="w-full text-sm text-gray-700 p-3 bg-white focus:outline-blue-400 rounded-sm"
+                                type="password"
                                 placeholder="Digite a sua senha"
                                 onChange={(e) => {
-                                    setLogin({...login, password: e.target.value})
-                                    setToaster({...toaster, show: false})
+                                    setLogin({ ...login, password: e.target.value })
+                                    setToaster({ ...toaster, show: false })
                                 }}
                             ></input>
                         </div>
 
                         <div className="flex flex-row gap-2 items-center mt-4">
-                        
+
                             <FormButtom label="Login" type="submit" />
 
                             {loading && (
@@ -95,30 +96,30 @@ async function handleSubmit(e: React.FormEvent)  {
 
                             )}
                         </div>
-                        <span className="text-xs mb-2 mt-2">Ainda não tem uma conta? 
+                        <span className="text-xs mb-2 mt-2">Ainda não tem uma conta?
                             <a href="request-register" className="text-xs text-blue-500"> Cadastrar</a>
                         </span>
                     </form>
                     <a href="forgot-password" className="text-xs text-blue-500">Esqueceu sua senha?</a>
                 </div>
-            </div>  
+            </div>
             <div className="sm:w-2/3 hidden sm:block flex w-full bg-neutral-50 h-screen items-center justify-center">
                 <div className="relative w-[100%] h-[100%] overflow-hidden shadow-lg">
                     <Image
                         src="/imgs/img_login.jpg"
                         alt="Foto - tela de login"
-                        fill 
+                        fill
                         className="object-cover"
                         priority
                     />
                 </div>
-            </div> 
+            </div>
             {toaster.show && (
-                <Toaster 
-                toaster={toaster}
-                setToaster={setToaster}
+                <Toaster
+                    toaster={toaster}
+                    setToaster={setToaster}
                 />
-            )} 
+            )}
         </div>
     )
 }
