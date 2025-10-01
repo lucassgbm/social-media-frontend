c19f48c48ea6ac43b91d42ccbed4297171833a6e
Gilvan Moraes de Moura <ti.gilvan@gmail.com>
2025-10-01 18:50:41 +0000

    Build do playground

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const url = "http://ip172-18-0-5-d3ej49291nsg00e4q000-80.direct.labs.play-with-docker.com/public/storage/";
+const url = "http://ip172-18-0-13-d3enaogl2o9000aqstjg-80.direct.labs.play-with-docker.com/public/storage/";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL(url + '**')],
