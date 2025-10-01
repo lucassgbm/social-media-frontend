24749329f953e42d2eac4de7d0ddefbc72d762de
Gilvan <ti.gilvan@gmail.com>
2025-10-01 11:37:50 -0300

    COnfigurando ambiente para build

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const url = "http://localhost:8080/public/storage/";
+const url = "http://ip172-18-0-5-d3ej49291nsg00e4q000-80.direct.labs.play-with-docker.com/public/storage/";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [new URL(url + '**')],
